cf4251753bf843473b30e7a861ad0dc6f58b8f96
Nicholas Shephard <shephardn@gmail.com>
2021-12-13 11:30:45 -0600

    commit tree 004dafbbe8cbcaa3e9af10a73002ca5cec362ed8

---
+++ app.rb
@@ -0,0 +1,9 @@
+# app.rb
+puts "Hello World!"
+# puts "Hello world!"
+# puts "Hello world!"
+# p [1, 2, 3]
+# puts [1, 2, 3].inspect
+# puts [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }].inspect
+puts "Pass this test, please."
+p [1, 2, 3]
